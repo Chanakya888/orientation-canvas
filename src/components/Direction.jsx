0cf43e7cb1501c51f59608b6d5fc7087522783b1
Chan <samuraichanakya1@gmail.com>
2023-05-08 23:58:40 +0530

    orientation

--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from "react";
 import ArrowSvg from "../assets/arrow.svg";
+import { AbsoluteOrientationSensor } from "../motion-src/motion-sensors";
 
 const Direction = () => {
   const [error, setError] = useState(null);
+  const [activate, setActivate] = useState(false);
   const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
-    if (window.DeviceOrientationEvent) {
-      window.addEventListener("deviceorientation", handleOrientation, true);
-    } else {
-      setError("Device orientation not supported");
-    }
-
-    return () => {
-      window.removeEventListener("deviceorientation", handleOrientation, true);
-    };
+    let orientation = new AbsoluteOrientationSensor({ frequency: 60 });
+    console.log("orientation is ", orientation);
+    orientation.onactivate(() => {
+      setActivate(true);
+    });
   }, []);
+  // useEffect(() => {
+  //   if (window.DeviceOrientationEvent) {
+  //     window.addEventListener("deviceorientation", handleOrientation, true);
+  //   } else {
+  //     setError("Device orientation not supported");
+  //   }
+
+  //   return () => {
+  //     window.removeEventListener("deviceorientation", handleOrientation, true);
+  //   };
+  // }, []);
 
   function handleOrientation(event) {
     let rotation = 0;
@@ -38,7 +47,7 @@ const Direction = () => {
         }}
       />
       <h1>{rotation.toFixed(2)}</h1>
-
+      <h2>activation {activate}</h2>
       <h2>{error}</h2>
     </div>
   );
