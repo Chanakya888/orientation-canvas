664c7e0b2a13f85391304f952d00d9afd3667ed6
Chan <samuraichanakya1@gmail.com>
2023-05-08 14:21:59 +0530

    feat: add arrow rot

--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import ArrowSvg from "../assets/arrow.svg";
 
 const Direction = () => {
   const [direction, setDirection] = useState("");
   const [error, setError] = useState(null);
+  const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
     if (navigator.geolocation) {
@@ -14,24 +16,22 @@ const Direction = () => {
             const alpha = event.alpha;
 
             let dir = "";
-            if (alpha >= 337.5 || alpha < 22.5) {
+            let rot = 0;
+            if (alpha >= 315 || alpha < 45) {
               dir = "North";
-            } else if (alpha >= 22.5 && alpha < 67.5) {
-              dir = "Northeast";
-            } else if (alpha >= 67.5 && alpha < 112.5) {
+              rot = 0;
+            } else if (alpha >= 45 && alpha < 135) {
               dir = "East";
-            } else if (alpha >= 112.5 && alpha < 157.5) {
-              dir = "Southeast";
-            } else if (alpha >= 157.5 && alpha < 202.5) {
+              rot = 90;
+            } else if (alpha >= 135 && alpha < 225) {
               dir = "South";
-            } else if (alpha >= 202.5 && alpha < 247.5) {
-              dir = "Southwest";
-            } else if (alpha >= 247.5 && alpha < 292.5) {
+              rot = 180;
+            } else if (alpha >= 225 && alpha < 315) {
               dir = "West";
-            } else {
-              dir = "Northwest";
+              rot = -90;
             }
             setDirection(dir);
+            setRotation(rot);
           }
         },
         (error) => {
@@ -39,12 +39,19 @@ const Direction = () => {
         }
       );
     } else {
-      setError("Geolocation is not supported by this browser.");
+      setError("Location access is not supported by this browser.");
     }
   }, []);
 
   return (
-    <div>{error ? <p>Error: {error}</p> : <p>Direction: {direction}</p>}</div>
+    <div>
+      <img
+        src={ArrowSvg}
+        alt="arrow"
+        style={{ transform: `rotate(${rotation}deg)` }}
+      />
+      {error ? <p>Error: {error}</p> : <p>Direction: {direction}</p>}
+    </div>
   );
 };
 
