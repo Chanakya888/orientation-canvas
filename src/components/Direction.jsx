c1eede24051b6ea54d0057993d27edc1911a326f
Chan <samuraichanakya1@gmail.com>
2023-05-08 15:10:26 +0530

    another try

--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -2,35 +2,46 @@ import React, { useState, useEffect } from "react";
 import ArrowSvg from "../assets/arrow.svg";
 
 const Direction = () => {
-  const [compassHeading, setCompassHeading] = useState(0);
-
-  const handleDeviceMotion = (event) => {
-    const { webkitCompassHeading, mozCompassHeading } = event;
-
-    const heading = webkitCompassHeading || mozCompassHeading;
-    if (heading) {
-      setCompassHeading(heading);
-    }
-  };
+  const [error, setError] = useState(null);
+  const [rotation, setRotation] = useState(0);
 
   useEffect(() => {
-    if (window.DeviceMotionEvent) {
-      window.addEventListener("devicemotion", handleDeviceMotion);
-    } else {
-      alert("Your device does not support DeviceMotion events.");
-    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          window.addEventListener("deviceorientation", handleOrientation);
 
-    return () => {
-      window.removeEventListener("devicemotion", handleDeviceMotion);
-    };
+          function handleOrientation(event) {
+            const alpha = event.alpha;
+
+            if (alpha !== null) {
+              const rotation = Math.round(alpha);
+              setRotation(rotation);
+            }
+          }
+        },
+        (error) => {
+          setError(error.message);
+        }
+      );
+    } else {
+      setError("Location access is not supported by this browser.");
+    }
   }, []);
 
   return (
     <div>
-      <div>
-        <h2>Phone's direction:</h2>
-        <p>{compassHeading.toFixed(2)}</p>
-      </div>
+      <img
+        src={ArrowSvg}
+        alt="arrow"
+        style={{
+          transform: `rotate(${-90 + rotation}deg)`,
+          transition: "transform 0.2s ease-out",
+        }}
+      />
+      <h1>{rotation.toFixed(2)}</h1>
+
+      <h2>{error}</h2>
     </div>
   );
 };
