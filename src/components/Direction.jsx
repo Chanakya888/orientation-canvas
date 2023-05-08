f8a3fa88cbca3820fad1c4db19dcca80dcff4bbd
Chan <samuraichanakya1@gmail.com>
2023-05-08 15:03:35 +0530

    try: with compass

--- src/components/Direction.jsx
+++ src/components/Direction.jsx
@@ -2,36 +2,35 @@ import React, { useState, useEffect } from "react";
 import ArrowSvg from "../assets/arrow.svg";
 
 const Direction = () => {
-  const [rotation, setRotation] = useState(0);
+  const [compassHeading, setCompassHeading] = useState(0);
 
-  const handleDeviceOrientation = (event) => {
-    const { alpha } = event;
-    setRotation(alpha);
+  const handleDeviceMotion = (event) => {
+    const { webkitCompassHeading, mozCompassHeading } = event;
+
+    const heading = webkitCompassHeading || mozCompassHeading;
+    if (heading) {
+      setCompassHeading(heading);
+    }
   };
 
   useEffect(() => {
-    if (window.DeviceOrientationEvent) {
-      window.addEventListener("deviceorientation", handleDeviceOrientation);
+    if (window.DeviceMotionEvent) {
+      window.addEventListener("devicemotion", handleDeviceMotion);
     } else {
-      alert("Your device does not support DeviceOrientation events.");
+      alert("Your device does not support DeviceMotion events.");
     }
 
     return () => {
-      window.removeEventListener("deviceorientation", handleDeviceOrientation);
+      window.removeEventListener("devicemotion", handleDeviceMotion);
     };
   }, []);
 
   return (
     <div>
-      <img
-        src={ArrowSvg}
-        alt="arrow"
-        style={{
-          transform: `rotate(${rotation}deg)`,
-          transition: "transform 0.2s ease-out",
-        }}
-      />
-      <h1>{rotation.toFixed(2)}</h1>
+      <div>
+        <h2>Phone's direction:</h2>
+        <p>{compassHeading.toFixed(2)}</p>
+      </div>
     </div>
   );
 };
